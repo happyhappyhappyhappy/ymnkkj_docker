e0c4fd4261693dda3df48d15911fcb574abca165
happyhappyhappyhappy <sakura@sakura>
2023-07-12 19:26:59 +0900

    first

---
+++ init.sh
@@ -0,0 +1,22 @@
+apt update \
+    && apt-get upgrade -y \
+    && apt-get install -y \
+    sudo \
+    bash \
+    zsh \
+    tini \
+    curl \
+    wget \
+    git \
+    python3 \
+    language-pack-ja-base \
+    language-pack-ja \
+    locales locales-all \
+    gnupg \
+    gnupg2 \
+    gnupg1 \
+    build-essential \
+    cmake \
+    clang \
+    libssl-dev \
+    vim
